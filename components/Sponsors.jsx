865f4d18ba878a6b83603a2f2e65c6e54198c174
SaiKiranMatta <saikirangoud.matta@gmail.com>
2023-10-01 22:35:03 +0530

    added co-sponsor interviewbuddy

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import React from "react";
 //import './Sponsors.css';
 import Image from "next/image";
+import Link from "next/link";
 const Sponsors = () => {
-  const sponsors = [
-    {
-      name: 'Sponsor 1',
-      logo: 'sponsor1.png',
-      website: 'https://www.sponsor1.com',
-    },
-    {
-      name: 'Sponsor 2',
-      logo: 'sponsor2.png',
-      website: 'https://www.sponsor2.com',
-    },
-  ];
+    return (
+        // <div className="sponsors-container">
+        //   <h2>Our Co-sponsor</h2>
+        //   <div className="sponsor-grid">
+        //     {sponsors.map((sponsor, index) => (
+        //       <div key={index} className="sponsor">
+        //         <a href={sponsor.website} target="_blank" rel="noopener noreferrer">
+        //           <Image width={200} height={200} src={sponsor.logo} alt={sponsor.name} />
+        //         </a>
+        //       </div>
+        //     ))}
+        //   </div>
+        // </div>
 
-  return (
-    <div className="sponsors-container">
-      <h2>Our Sponsors</h2>
-      <div className="sponsor-grid">
-        {sponsors.map((sponsor, index) => (
-          <div key={index} className="sponsor">
-            <a href={sponsor.website} target="_blank" rel="noopener noreferrer">
-              <Image width={200} height={200} src={sponsor.logo} alt={sponsor.name} />
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        <div className="flex flex-col items-center gap-8">
+            <div className="flex flex-col items-center gap-2 ">
+                <h1 className="justify-center text-4xl text-center justify-items-center text-bblue-200 shimmerb">
+                    Our Co-sponsor
+                </h1>
+                <div className=" h-[2px] bg-bblue-200 w-16"></div>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+                <Link href="https://interviewbuddy.in/" target="_blank">
+                    <Image
+                        src="/images/sponsors/interview-buddy.png"
+                        width={284}
+                        height={160}
+                        className="rounded-xl"
+                    />
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Sponsors;
-
